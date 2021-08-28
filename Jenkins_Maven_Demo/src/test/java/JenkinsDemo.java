7cb2e846a79c267e06ea990add1cca667467b173
java-tejaswini <45378471+java-tejaswini@users.noreply.github.com>
2021-08-28 12:50:08 +0530

    Update JenkinsDemo.java
    link

--- Jenkins_Maven_Demo/src/test/java/JenkinsDemo.java
+++ Jenkins_Maven_Demo/src/test/java/JenkinsDemo.java
@@ -6,6 +6,7 @@ import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.WebElement;
 
 public class JenkinsDemo
 {
@@ -37,6 +38,21 @@ public class JenkinsDemo
     public void testCaseFailed()
     {
         //Assert.assertTrue(driver.findElement(By.xpath("//form[@id='failed case']")).isDisplayed());
+        
+        driver.get("https://www.linkedin.com/login");
+        
+        WebElement username=driver.findElement(By.id("username"));
+        WebElement password=driver.findElement(By.id("password"));
+        WebElement login=driver.findElement(By.xpath("//button[text()='Sign in']"));
+        
+        username.sendKeys("example@gmail.com");
+        password.sendKeys("password");
+        login.click();
+        
+        String actualUrl="https://www.linkedin.com/feed/";
+        String expectedUrl= driver.getCurrentUrl();
+        
+        Assert.assertEquals(expectedUrl,actualUrl);
         
     }
 
